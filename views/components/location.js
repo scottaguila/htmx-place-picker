852cf20479ad2feb0f80992f7ba6020cead70253
scottaguila <scott.aguila@gmail.com>
2024-04-01 11:34:58 -0400

    Added hx-vals to button in order to send location ID to post handler

--- views/components/location.js
+++ views/components/location.js
@@ -1,8 +1,12 @@
 export default function renderLocation(location) {
   return `
     <li class="location-item">
-      <button>
-        <img src="${`/images/${location.image.src}`}" alt="${location.image.alt}" />
+      <button 
+        hx-post="/places" 
+        hx-vals='{"locationId": "${location.id}"}'>
+        <img src="${`/images/${location.image.src}`}" alt="${
+    location.image.alt
+  }" />
         <h3>${location.title}</h3>
       </button>
     </li>
