c3128f5e95fae9bf1bd9b94aacf23492a00619fe
scottaguila <scott.aguila@gmail.com>
2024-04-01 11:03:46 -0400

    Added comments to help understand code

--- app.js
+++ app.js
@@ -11,30 +11,39 @@ const INTERESTING_LOCATIONS = [];
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
+// Receives GET request and renders locations page
+// with updated lists of available and interesting locations
 app.get('/', (req, res) => {
+  // An array containing all locations NOT in INTERESTING_LOCATIONS
   const availableLocations = AVAILABLE_LOCATIONS.filter(
-    (location) => !INTERESTING_LOCATIONS.includes(location)
+    location => !INTERESTING_LOCATIONS.includes(location)
   );
   res.send(renderLocationsPage(availableLocations, INTERESTING_LOCATIONS));
 });
 
+// Receives POST request with locationId and
+// pushes it to INTERESTING_LOCATIONS
 app.post('/places', (req, res) => {
   const locationId = req.body.locationId;
-  const location = AVAILABLE_LOCATIONS.find((loc) => loc.id === locationId);
+  // Returns location with matching id
+  const location = AVAILABLE_LOCATIONS.find(loc => loc.id === locationId);
   INTERESTING_LOCATIONS.push(location);
-
+  // Why not redirect to '/', which would call renderLocationsPage?
   res.send(`
     TODO
   `);
 });
 
+// Receives DELETE request with locationId and
+// removes it from INTERESTING_LOCATIONS
 app.delete('/places/:id', (req, res) => {
   const locationId = req.params.id;
   const locationIndex = INTERESTING_LOCATIONS.findIndex(
-    (loc) => loc.id === locationId
+    loc => loc.id === locationId
   );
   INTERESTING_LOCATIONS.splice(locationIndex, 1);
-
+  // Sends back empty response, which will replace the
+  // element that sent the DELETE request
   res.send();
 });
 
