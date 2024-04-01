8635cd6d88b832cac99ee057d813bc3dbe4a2b52
scottaguila <scott.aguila@gmail.com>
2024-04-01 12:09:38 -0400

    Added conditional htmx attributes based on image location to make deleting images possible

--- views/components/location.js
+++ views/components/location.js
@@ -1,11 +1,25 @@
-export default function renderLocation(location) {
+export default function renderLocation(location, isAvaiableLocation = true) {
+  let attributes;
+
+  if (isAvaiableLocation) {
+    attributes = `
+      hx-post="/places" 
+      hx-vals='{"locationId": "${location.id}"}'
+      hx-target="#interesting-locations"
+      hx-swap="beforeend"
+    `;
+  } else {
+    attributes = `
+      hx-delete="/places/${location.id}"
+      hx-confirm="Are you sure you want to delete this location?"
+      hx-target="closest li"
+      hx-swap="outerHTML"
+    `;
+  }
+
   return `
     <li class="location-item">
-      <button 
-        hx-post="/places" 
-        hx-vals='{"locationId": "${location.id}"}'
-        hx-target="#interesting-locations"
-        hx-swap="beforeend">
+      <button ${attributes}>
         <img 
           src="${`/images/${location.image.src}`}" 
           alt="${location.image.alt}" />
