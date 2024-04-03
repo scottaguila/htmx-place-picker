078ea168f0b4a4deed5b0bc137bfbb568c32279b
scottaguila <scott.aguila@gmail.com>
2024-04-02 20:10:18 -0400

    Created modal

--- public/main.js
+++ public/main.js
@@ -1,5 +1,32 @@
-function showConfirmationModal() {
-  console.log('showing modal...');
+function showConfirmationModal(event) {
+  event.preventDefault();
+  console.log(event);
+  const confirmationModal = `
+        <dialog class="modal">
+            <div id="confirmation">
+                <h2>Are you sure?</h2>
+                <p>
+                    You can always add it back later.
+                </p>
+                <div id="confirmation-actions">
+                    <button 
+                        id="action-no"
+                        className="button-text">
+                            Cancel
+                    </button>
+                    <button 
+                        id="action-yes"
+                        className="button">
+                            Yes
+                    </button>
+                <div>
+            <div>
+        </dialog>`;
+
+  document.body.insertAdjacentHTML('beforeend', confirmationModal);
+  //   Manually show modal
+  const dialog = document.querySelector('dialog');
+  dialog.showModal();
 }
 
 // Add event listener to dcoument because htm:beforeRequest on
